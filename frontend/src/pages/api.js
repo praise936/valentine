c085ed90a3d99ce2f84312a308461e8a025ea6a7
praise936 <peterpraise259@gmail.com>
2026-01-26 23:52:29 +0300

    Configure API base URL

--- frontend/src/pages/api.js
+++ frontend/src/pages/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const API_BASE = import.meta.env.VITE_API_BASE_URL;
 const api = axios.create({
-    baseURL: "http://localhost:8000/api/",
+    baseURL: API_BASE,
     
 });
 
